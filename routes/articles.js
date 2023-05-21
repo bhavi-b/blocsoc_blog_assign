fa229124bfc17b42463636b968db9165155f7786
Bhavishay Bharti <bhavishaybharti@gmail.com>
2023-05-21 22:28:58 +0530

    Revert "not working"
    This reverts commit 1a6d3ad3e39d29daf5c645d1eb4951e80298c5f3.

--- routes/articles.js
+++ routes/articles.js
@@ -6,8 +6,8 @@ router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() })
 })
 
-router.get("/:slug", async (req, res) => {
-  const article = await Article.findOne({ slug: req.params.slug })
+router.get("/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id)
   if (article == null) {
     res.redirect("/")
   }
@@ -22,15 +22,10 @@ router.post("/", async (req, res) => {
   })
   try {
     article = await article.save()
-    res.redirect(`/articles/${article.slug}`)
+    res.redirect(`/articles/${article.id}`)
   } catch (e) {
     res.render("/articles/new", { article: article })
   }
-})
-
-router.delete("/:id", async (req, res) => {
-  await Article.findByIdAndDelete(req.params.id)
-  res.redirect("/")
 })
 
 module.exports = router
