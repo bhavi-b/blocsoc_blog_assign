a8adb41cf2e728891b509accd4b03448c77a17ee
Bhavishay Bharti <bhavishaybharti@gmail.com>
2023-05-21 21:04:18 +0530

    moved index file and created schema

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
 const articleRouter = require("./routes/articles")
 const app = express()
 
+mongoose.connect(
+  `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.ns0ueu5.mongodb.net/?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
 app.set("view engine", "ejs")
 
-app.use("/articles", articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
   const articles = [
     {
       title: "test article",
-      createdAt: Date.now(),
+      createdAt: new Date(),
       description: "test description",
     },
     {
       title: "test article 2",
-      createdAt: Date.now(),
+      createdAt: new Date(),
       description: "test description 2",
     },
   ]
-  res.render("index", { articles: articles })
+  res.render("articles/index", { articles: articles })
 })
+
+app.use("/articles", articleRouter)
 
 app.listen(5000)
